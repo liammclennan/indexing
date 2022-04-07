12827b867bf5f85694dcc215dba997f8d3a8888f
Liam McLennan <liam.mclennan@gmail.com>
2022-04-07 21:04:06 +1000

    small db, large db and large indexed db

--- database.mjs
+++ database.mjs
@@ -1,10 +1,12 @@
 import logger from './logger.mjs';
-import lines from './data/index.mjs';
+import big_database from './data/index.mjs';
 
 const contains = (big_string, little_string) => {
   const exp = new RegExp(`^.*\\b${little_string}\\b.*`, 'i');
   return exp.test(big_string);
 };
+
+const search_term = process.argv[2]; // node database.mjs 'and nothing'
 
 const small_database = [
   `Call me Ishmael. Some years ago—never mind how long precisely—having`,
@@ -13,28 +15,89 @@ const small_database = [
   `of the world. `
 ];
 
-const find_lines_containing = (data, term, index) => {
+let time_and_log = (f) => {
   const start = process.hrtime();
-  const result = (index) 
-    ? index.map(i => data[i])
-    : data.filter(line => contains(line, term));
+  const result = f();
   const timing = process.hrtime(start);
-  logger.info("Found {Occurrances} occurrances of {Found} in {Elapsed}us. Searched {Lines} lines.", {     
+  const message = result.message;
+  delete result.message;
+  logger.info(message, result);
+} 
+
+let find_lines_containing = (data, term) => {
+  const start = process.hrtime();
+  const result = data.filter(line => contains(line, term));
+  const timing = process.hrtime(start);
+  return {
+    message: "Small database search. Found {Occurrances} occurrances of {Found} in {Elapsed}us. Searched {Lines} lines.",     
     Occurrances: result.length,
     Found: term, 
     Elapsed: timing[1]/1000, 
     Result: result.slice(0,2),
     Lines: data.length
-  });
-  return result;
-}
+  };
+};
+
+time_and_log(find_lines_containing.bind(this, small_database, search_term));
+
+
+
+
+
+
+
+
+
+/*
+
+*/
+find_lines_containing = (data, term) => {
+  const start = process.hrtime();
+  const result = data.filter(line => contains(line, term));
+  const timing = process.hrtime(start);
+  return {
+    message: "No index search. Found {Occurrances} occurrances of {Found} in {Elapsed}us. Searched {Lines} lines.",
+    Occurrances: result.length,
+    Found: term, 
+    Elapsed: timing[1]/1000, 
+    Result: result.slice(0,2),
+    Lines: data.length
+  };
+};
+
+time_and_log(find_lines_containing.bind(this, big_database, search_term));
+
+
+
+
+
+
+/*
+
+
+*/
+find_lines_containing = (data, term, index) => {
+  const start = process.hrtime();
+  const result = (index) 
+    ? index.map(i => data[i])
+    : data.filter(line => contains(line, term));
+  const timing = process.hrtime(start);
+  return {
+    message: "Indexed search. Found {Occurrances} occurrances of {Found} in {Elapsed}us. Searched {Lines} lines.",
+    Occurrances: result.length,
+    Found: term, 
+    Elapsed: timing[1]/1000, 
+    Result: result.slice(0,2),
+    Lines: data.length
+  };
+};
 
 const build_index_for = (term) => {
-  return lines.reduce((p, c, i) => {
+  return big_database.reduce((p, c, i) => {
     return contains(c, term) ? p.concat([i]) : p;
   }, []);
 };
 
-const whale_index = build_index_for('whale');
-find_lines_containing(lines, 'whale', whale_index);
+const term_index = build_index_for(search_term);
 
+time_and_log(find_lines_containing.bind(this, big_database, search_term, term_index));
--- logger.mjs
+++ logger.mjs
@@ -3,18 +3,16 @@ import { SeqTransport } from '@datalust/winston-seq';
 
 const logger = winston.createLogger({
   level: 'info',
-  format: winston.format.combine(  /* This is required to get errors to log with stack traces. See https://github.com/winstonjs/winston/issues/1498 */
+  format: winston.format.combine(
     winston.format.errors({ stack: true }),
     winston.format.json(),
   ),
-  defaultMeta: { /* application: 'your-app-name' */ },
   transports: [
     new winston.transports.Console({
       format: winston.format.simple(),
     }),
     new SeqTransport({
       serverUrl: "http://localhost",
-    //   apiKey: "your-api-key",
       onError: (e => { console.error(e) }),
       handleExceptions: true,
       handleRejections: true,
